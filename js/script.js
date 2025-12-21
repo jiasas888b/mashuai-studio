c43e98a74a016a1f69711286edb62732f1dde3b6
jiasas888b <jiasas352@gmail.com>
2025-12-21 20:41:05 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-
+//打开窗口或页面
+function MSFS(){
+    window.open("MSF2.html");
+}
+function MSR(){
+    window.open("MSR.html");
+}
+function HP(){
+    window.open("HoleP.html");
+}
+function lhx(){
+    window.open("lhx.html");
+}
+function HTS(){
+    window.open("HTS.html");
+}
